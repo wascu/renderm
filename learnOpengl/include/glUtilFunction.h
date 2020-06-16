c6a1de23ca00c0b6c20432668adb51ae1e745092
tao.q <tao.qiu@megatronix.co>
2020-06-16 16:22:34 +0800

    add util func

--- learnOpengl/include/glUtilFunction.h
+++ learnOpengl/include/glUtilFunction.h
@@ -5,13 +5,15 @@
 #ifndef LEARNOPENGL_GLUTILFUNCTION_H
 #define LEARNOPENGL_GLUTILFUNCTION_H
 
+#include <jmorecfg.h>
 #include "std_afx.h"
 
 NAMESPACE_GL_BEGIN
 //for loading a 2D texture from file
 unsigned int loadTexture(char const *path,float& pic_width,float& pic_height);
 
-
+// Generates a texture that is suited for attachments to a framebuffer
+unsigned int generateAttachmentTexture(boolean depth, boolean stencil,unsigned int width,unsigned int height);
 NAMESPACE_END
 
 
--- learnOpengl/src/glUtilFunction.cpp
+++ learnOpengl/src/glUtilFunction.cpp
@@ -46,3 +46,29 @@ unsigned int ns_MG_GL::loadTexture(char const *path,float& pic_width,float& pic_
 
     return textureID;
 }
+
+unsigned int ns_MG_GL::generateAttachmentTexture(boolean depth, boolean stencil,
+        unsigned int width,unsigned int height) {
+    // What enum to use?
+    GLenum attachment_type;
+    if(!depth && !stencil)
+        attachment_type = GL_RGB;
+    else if(depth && !stencil)
+        attachment_type = GL_DEPTH_COMPONENT;
+    else if(!depth && stencil)
+        attachment_type = GL_STENCIL_INDEX;
+
+    //Generate texture ID and load texture data
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    if(!depth && !stencil)
+        glTexImage2D(GL_TEXTURE_2D, 0, attachment_type, width, height, 0, attachment_type, GL_UNSIGNED_BYTE, NULL);
+    else // Using both a stencil and depth test, needs special format arguments
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width, height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return textureID;
+}
